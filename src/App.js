09a909ce26036e0e4a112924741ee65d3af4ec30
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-07-07 16:39:17 +0300

    Page layout set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import Sidebar from "./Component/SideBar/SideBar";
+import "./App.css"
 const App = () => {
   return (
-    <h1>React Dashboard</h1>
+    <div className="container">
+      <nav>
+        <Sidebar />
+      </nav>
+      <main>
+        <h1>deneme</h1>
+        <h1>deneme2</h1>
+      </main>
+    </div>
+
   );
 }
 
