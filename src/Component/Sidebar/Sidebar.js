ab8dbc7101d59e8828ce00823f9b259a711412e9
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-07-07 18:06:00 +0300

    Edited in HTML SideBarMenu Component Added

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,3 +1,5 @@
+import SideBarMenu from '../SideBarMenu.js/SideBarMenu';
+import "./SideBar.css"
 import {
     AiOutlineHome,
     AiOutlineAppstore,
@@ -40,29 +42,32 @@ const Sidebar = () => {
 
     ]
 
-
     return (
-        <div className='sidebar'>
-            <div className='sidebar_content'>
-                <div className="top_sidebar">
-                    <div className='logo-icon'>
-
+        <section>
+            <div className='sidebar'>
+                <div className='sidebar_content'>
+                    <div className="top_sidebar">
+                        <div className='logo-icon'>
+                            <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle className='big-icon' cx="12" cy="16" r="10" stroke="#0075FF" strokeWidth="4" />
+                                <circle className='small-icon' cx="25" cy="26" r="6" stroke="#1BE3A7" strokeWidth="4" />
+                            </svg>
+                        </div>
+                        <h1 className='logo-text'>ChirKuut</h1>
+                        <div className='bars'> <AiOutlineMenuFold /> </div>
                     </div>
-                    <h1 className='logo-text'>ChirKuut</h1>
-                    <div className='bars'>
 
-                    </div>
+                    {menuItems.map((item, index) => <SideBarMenu key={index} name={item.name} icon={item.icon} />)}
+
+                </div>
+
+                <div className='logout'>
+                    <SideBarMenu name="Log out" icon={<AiOutlineLogin />} />
                 </div>
 
             </div>
 
-            <div className='logout'>
-
-            </div>
-
-
-
-        </div>
+        </section>
     );
 }
 
